025a36605b1f0a89f1820f403600fce923be97a3
Amiya Pani <29244033+ampani13@users.noreply.github.com>
2022-05-09 13:14:53 +0530

    Update streamlit_app.py
    changes app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,11 @@ streamlit.header('Amiya\'s Pros:')
 streamlit.text('smart')
 streamlit.text('intelligent')
 streamlit.text('helpful')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
